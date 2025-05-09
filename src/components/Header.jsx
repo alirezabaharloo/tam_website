6ccc84c0f1f5918e992e7e0208f0b88862afc3ad
Salar <SaliTech.2008@gmail.com>
2025-05-09 20:16:21 +0330

    HeaderUpdate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -143,16 +143,14 @@ export default function Header() {
                         </div>
                     </div>
                     <div className="h-12 w-px bg-[#F2FAFF] opacity-50"></div>
-                    <button onClick={handleAccountClick} className="flex items-center group focus:outline-none">
+                    <button onClick={handleAccountClick} className="flex items-center group h-12 focus:outline-none">
                       {user ? (
                         <>
-                          <span className="text-quinary-tint-800 text-[20px] font-normal mr-1" style={{marginLeft: 4}}>{user.phone}</span>
-                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 transition-colors duration-200 group-hover:text-quinary-tint-600 text-quinary-tint-800">
-                            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                          </svg>
+                          <span className="text-quinary-tint-800 text-[20px] font-normal mr-1">{user.phone}</span>
+                          <img src="/images/icons/UserLogo2.svg" alt="User" className="w-[32px] h-[32px] transition-colors duration-200 group-hover:text-quinary-tint-600 text-quinary-tint-800" />
                         </>
                       ) : (
-                        <img src="/images/icons/UserLogo.svg" alt="User" className="w-6 h-6 transition-colors duration-200 group-hover:text-quinary-tint-600 text-quinary-tint-800" />
+                        <img src="/images/icons/UserLogo.svg" alt="User" className="w-[23px] h-[30px] transition-colors duration-200 group-hover:text-quinary-tint-600 text-quinary-tint-800" />
                       )}
                     </button>
                     <div className="h-12 w-px bg-[#F2FAFF] opacity-50"></div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Slider from '../components/Slider';
 import LatestNews from '../components/LatestNews';
 import LatestVideos from '../components/LatestVideos';
 import TeamBoxes from '../components/TeamBoxes';
 import ShopSection from '../components/ShopSection';
 import PlayerSection from '../components/PlayerSection';
-import Modal from '../components/Modal';
 
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    setIsModalOpen(true);
-  }, []);
-
-  const modalData = {
-    title: "Welcome To Our Website",
-    content: "Welcome to TamSport! We're excited to have you here. Explore our latest news, videos, and team updates. Enjoy your visit!",
-    onlyAcceptable: false
-  };
-
   return (
     <div className="relative mt-4">
-      <Modal 
-        isOpen={isModalOpen} 
-        onClose={() => setIsModalOpen(false)} 
-        data={modalData}
-      />
       <div id="slider">
         <Slider />
       </div>
